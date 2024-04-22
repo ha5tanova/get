ac513c77dbcd2c143b495e74808d6bc7e7f0b2da
haptanova03@gmail.com <haptanova03@gmail.com>
2024-04-22 23:21:32 +0800

    решение для задачи

--- main.js
+++ main.js
@@ -5,3 +5,18 @@
  - Массив
  - Индекс
  - Значение по умолчанию (равно null) */
+
+
+ const get = (array, index, defaultValue = null) => {
+    return index >= 0 && index < array.length ? array[index] : defaultValue;
+}
+
+const cities = ['ulan-ude', 'seoul', 'irkutsk', 'moscow', '', null, undefined];
+
+console.log(get(cities, 1)); // 'london'
+console.log(get(cities, 4)); // ''
+console.log(get(cities, 10, 'paris')); // 'paris'
+console.log(get(cities, -1, 'oops')); // 'oops'
+console.log(get(cities, 5, 'oops')); // null
+console.log(get(cities, 6, 'oops')); // undefined
+console.log(get(cities, 7)); // null
